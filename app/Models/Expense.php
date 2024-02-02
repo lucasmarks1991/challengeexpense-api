de2aa9a785893d72dc43fd61d1103f13952a1972
Lucas Marques Santos <lucasmarks1991@hotmail.com>
2024-02-01 22:54:21 -0300

    style: adicionado helpers nos models

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,11 +2,35 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\Expense
+ *
+ * @property int $id
+ * @property string $description
+ * @property \Illuminate\Support\Carbon $occurred_date
+ * @property int $user_id
+ * @property string $currency
+ * @property string $value
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense whereCurrency($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense whereOccurredDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Expense whereValue($value)
+ * @mixin \Eloquent
+ */
 class Expense extends Model
 {
     use HasFactory;
